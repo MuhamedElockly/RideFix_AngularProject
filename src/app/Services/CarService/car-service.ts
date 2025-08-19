2d741b3c0b52e670cdbf3108333c5ae630dcb695
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-08-19 22:19:39 +0300

    Edit

--- src/app/Services/CarService/car-service.ts
+++ src/app/Services/CarService/car-service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ICarResponse } from '../../Interfaces/Car/car-response';
 import { ApiConfigService } from '../api-config.service';
+import { CreateCarInterface } from '../../Interfaces/Car/create-car-interface';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,13 @@ export class CarService {
     return this.clientService.get<ICarResponse>(
       this.apiConfig.getApiUrl('Car')
     );
+  }
+
+  CreateMyCar(car: CreateCarInterface): Observable<any> {
+    return this.clientService.post<any>('http://localhost:5038/api/Car', car);
+  }
+
+  DeleteCar(): Observable<any> {
+    return this.clientService.delete<any>(`http://localhost:5038/api/Car`);
   }
 }
